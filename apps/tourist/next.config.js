c8b24844ced66132c50c3156126b8399d32823d2
DanLigairi1978 <ligarius22@gmail.com>
2026-03-11 23:54:12 +1200

    Fix: COOP header for Google Sign-In popup

--- apps/tourist/next.config.js
+++ apps/tourist/next.config.js
@@ -58,6 +58,21 @@ const nextConfig = {
             },
         ],
     },
+
+async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Cross-Origin-Opener-Policy',
+                        value: 'same-origin-allow-popups',
+                    },
+                ],
+            },
+        ];
+    },
+
     async redirects() {
         return [
             {
